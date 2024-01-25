410794d9db6d2cc5142e833bdafd6eddf6d5e608
rauldeving <dlaviniuraul@gmail.com>
2024-01-25 16:39:33 +0200

    Added data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Kaerus.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
