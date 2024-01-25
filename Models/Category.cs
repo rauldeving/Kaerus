8ef1fd5901055521ba465a83cec748e3f6f262b4
rauldeving <dlaviniuraul@gmail.com>
2024-01-25 16:24:00 +0200

    Added NuGGet

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace Kaerus.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kaerus.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
 
         public string DisplayOrder { get; set; }
